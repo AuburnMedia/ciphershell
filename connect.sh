022f4fb8c920e1c32244e6fa42919f9ebcd9c9c6
AuburnMedia <nicholas.houghton@smseymour.catholic.edu.au>
2024-05-20 15:53:37 +1000

    cleaned connect.sh and added notification logic

--- connect.sh
+++ connect.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-#
+source ./functions.sh
+
+clear
+displaynotification "The VPN is now connecting. Do not turn off or restart your machine." "Connecting.."
+
+displaydialog "The VPN is now connected. Please note, if this is your first time running this script, you must restart your computer for changes to take effect."
