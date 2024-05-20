2323ce5175331639d8f4fbd545f0724f1aac683d
AuburnMedia <nicholas.houghton@smseymour.catholic.edu.au>
2024-05-20 15:58:48 +1000

    added progressbar logic to functions.sh

--- functions.sh
+++ functions.sh
@@ -52,3 +52,22 @@ displayfortext() { # $1: message $2: default text
 	    exit 1
 	fi
 }
+
+# end credit
+# credit edouard-lopez/progress-bar.sh
+progress-bar() {
+  local duration=${1}
+
+
+    already_done() { for ((done=0; done<$elapsed; done++)); do printf "▇"; done }
+    remaining() { for ((remain=$elapsed; remain<$duration; remain++)); do printf " "; done }
+    percentage() { printf "| %s%%" $(( (($elapsed)*100)/($duration)*100/100 )); }
+    clean_line() { printf "\r"; }
+
+  for (( elapsed=1; elapsed<=$duration; elapsed++ )); do
+      already_done; remaining; percentage
+      sleep 1
+      clean_line
+  done
+  clean_line
+}
