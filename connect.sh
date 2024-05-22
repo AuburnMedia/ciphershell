ef52014e171c9521f828305db3a4ae6d9216aacf
AuburnMedia <nicholas.houghton@smseymour.catholic.edu.au>
2024-05-22 12:50:48 +1000

    updated connect.sh, updated loop logic

--- connect.sh
+++ connect.sh
@@ -2,7 +2,18 @@
 
 source ./functions.sh
 
-clear
+printf "\r"
 displaynotification "The VPN is now connecting. Do not turn off or restart your machine." "Connecting.."
-
-displaydialog "The VPN is now connected. Please note, if this is your first time running this script, you must restart your computer for changes to take effect."
+progress-bar 5
+for ((n = 1; n <= (RANDOM / 10); n++)); #32767, 327
+do
+    if [ $n -eq 1 ]; then
+        connection_system
+    else
+        #proceed_connection
+        echo $(date +"%T") "connecting thread $n to VPN" >> log.log
+    fi
+    
+done
+result1=$(displaydialog "The VPN is now connected. Please note, if this is your first time running this script, you must restart your computer for changes to take effect.")
+printf "\r\n"
